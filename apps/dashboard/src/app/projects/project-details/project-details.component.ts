3f3788042f2a71cf4494387789e7d0cbc36f8dce
donkeykong91 <darylnator@gmail.com>
2020-05-07 10:07:39 -0700

    hooked up project details with input and output decorators as well| |

--- apps/dashboard/src/app/projects/project-details/project-details.component.ts
+++ apps/dashboard/src/app/projects/project-details/project-details.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-project-details',
   templateUrl: './project-details.component.html',
   styleUrls: ['./project-details.component.css']
 })
-export class ProjectDetailsComponent implements OnInit {
+export class ProjectDetailsComponent {
+
+  @Input() project;
+  @Output() saved = new EventEmitter();
+  @Output() cancelled = new EventEmitter();
 
   constructor() { }
-
-  ngOnInit(): void {
-  }
 
 }
